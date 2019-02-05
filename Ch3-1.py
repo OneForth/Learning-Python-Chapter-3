2b9ee781ad48fa12ad256e9d650e8cf6dcf4d0d5
OneForth <47368292+OneForth@users.noreply.github.com>
2019-02-05 14:21:26 -0700

    Add files via upload

---
+++ Ch3-1.py
@@ -0,0 +1,10 @@
+print("This program will add two numbers together of your choice and print out the sum.\nLets begin!")
+#Just a print statment letting the user know what this program is for.
+
+
+value1 = eval(input("Please enter a number: "))
+value2 = eval(input("Please enter another number"))
+#Gathers input from the user using an input function inside a eval fuction
+
+print(value1, "+", value2, "=", value1 + value2)
+
